6f6b94d05f4c837aecade4ad1704596410610859
farmerrrrr <74737416+farmerrrrr@users.noreply.github.com>
2022-12-20 00:21:51 +0900

    [3.ProxyContract] feat: 3.ProxyContract 업로드

---
+++ 3.ProxyContract/contracts/UUPS_ProxyV2.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract ProxyContractV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    uint256 public count;
+
+    function initialize() public initializer {
+        count = 10;
+        _Ownable_init();
+    }
+
+    function _authorizeUpgrade(address) internal override onlyOwner { }
+    
+    function increase() external {
+        count++;
+    }
+}
+
+contract ProxyContractV2 is ProxyContractV1 {
+    function decrease() external {
+        count--;
+    }
+}
