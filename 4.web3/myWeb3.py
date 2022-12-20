f4f1a345698931313615160e278bf3362a4a479d
farmerrrrr <74737416+farmerrrrr@users.noreply.github.com>
2022-12-21 00:27:57 +0900

    [4.web3] feat: 최신 블록 조회

--- 4.web3/myWeb3.py
+++ 4.web3/myWeb3.py
@@ -1,10 +1,23 @@
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
 import json
+from hexbytes import HexBytes
+
+class HexJsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, HexBytes):
+            return obj.hex()
+        return super().default(obj)
 
 w3 = Web3(Web3.HTTPProvider('https://goerli.infura.io/v3/01ff5dcf66ce4347989a1ed3fd74c6e1'))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 print("############################")
 print(w3.isConnected())
+print("############################")
+
+print("Get last block")
+latestBlock = w3.eth.get_block('latest')
+print("############################")
+print(json.dumps(dict(latestBlock), cls=HexJsonEncoder))
 print("############################")
